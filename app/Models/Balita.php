9bd286d16528486fb2b883a21ed11b93a0e1239c
FaisalRhdn <faisalrahma531@gmail.com>
2023-11-26 15:44:06 +0700

    Tambah PriM eloquent models balita

--- app/Models/Balita.php
+++ app/Models/Balita.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Balita extends Model
 {
     use HasFactory;
+    protected $primaryKey = 'id_balita';
+    public $incrementing = false;
+
+    protected $casts = [
+        'bb_lahir' => 'decimal:1',
+        'pb_lahir' => 'decimal:1'
+    ];
 }
